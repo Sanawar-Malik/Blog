2cf3f1a2a859a9bc99fbf053d4643379807d443d
Sanawar78 <sanawar78m@gmail.com>
2022-01-17 11:31:44 +0500

    initial commit ok

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def register_page():
         db.session.commit()
         login_user(user_to_create)
         flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
-        return redirect(url_for('login_page'))
+        return redirect(url_for('home'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
@@ -258,13 +258,13 @@ def account():
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
         current_user.username = form.username.data
-        current_user.email = form.email.data
+        current_user.email_address = form.email.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
         form.username.data = current_user.username
-        form.email.data = current_user.email
+        form.email.data = current_user.email_address
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',image_file=image_file, form=form)
 
